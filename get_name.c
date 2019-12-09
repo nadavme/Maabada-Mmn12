41effde0fd25a33fab77370049f6af6ef0d9bc3e
Nadav Meidan <nadav.medan@mail.huji.ac.il>
2019-12-09 15:21:29 +0200

    Initial commit of the project

--- get_name.c
+++ get_name.c
@@ -2,13 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+char NAMES_LIST[30];
 
+
+
+/*!
+ *
+ * @return
+ */
 char get_name()
 {
-
-
+    return NAMES_LIST[rand() % 30];
 }
 
+
+/*!
+ *
+ * @return
+ */
 int main() {
     printf("Hello, World!\n");
     return 0;
