fefba93e7951f3b2305cf33a57ae64d4cd44de25
nadav.meidan <nadav.medan@mail.huji.ac.il>
2019-12-09 14:33:09 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Mmn12.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mmn12.dir/main.c.o"
+  "Mmn12.pdb"
+  "Mmn12"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Mmn12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ get_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char get_name()
+{
+
+
+}
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
