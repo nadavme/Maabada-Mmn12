ee004583287f6dde05fb6a8885134b4a0269aa23
nadav.meidan <nadav.medan@mail.huji.ac.il>
2019-12-17 13:03:32 +0200

    having problem with fgets

--- get_name.c
+++ get_name.c
@@ -1,59 +1,71 @@
 
 #include "get_name.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
-#include <ctype.h>
 
-char NAMES_LIST[30];
+
+
 
 
 
 /*!
- *
+ *See header file.
  * @return
  */
 char get_name()
 {
+    srand(time(NULL));
     return NAMES_LIST[rand() % 30];
 }
 
-bool contains(char str, int i)
+/*!
+ * See header file.
+ * @param str
+ * @param i
+ * @return
+ */
+bool contains(char str[], int i)
 {
     int j;
     for (j = 0; j < i; j++)
     {
-        if(NAMES_LIST[j] == str) return false;
+        if(NAMES_LIST[j] == str) return true;
     }
-    return true;
+    return false;
 }
 
 
 /*!
- *
+ *Runs the program.
  * @return
  */
 int main() {
     char str[20];
-    int i, j= 0;
-    for (i=0; i < 30; i++)
+    unsigned long j;
+    int i, k= 0;
+    printf("Please enter 30 names: ");
+    fgets(str, 300, stdin);
+    printf("\nThe names that was accepted correctly are:\n%s", str);
+    /*ensure 'str' doesn't holds '\n' or ' '*/
+    if(str[strlen(str)-1] == '\n' || str[strlen(str)-1] == ' ') str[strlen(str)-1] = NULL_TERMINATED_STRING;
+    for(j = 0; j <  strlen(str); j++)
     {
-        fgets(str, 20, stdin);
-        for(j = 0; str[j]; j++)
+        str[j] = tolower(str[j]);
+    }
+    /*Checks if the name was already accepted as an input, before adding it to the list*/
+    while(k < 30)
+    {
+        for (i=0; i < k; i++)
         {
-            str[j] = tolower(str[j]);
-        }
-
-        if(contains((char) str, i))
-        {
-            printf("%s", "This name was already accepted. The program will shut down now");
-            exit(11);
+            if (contains(str, i))
+            {
+                printf("\nOne of the names was entered twice. The program will shut down now");
+                exit(11);
+            }
         }
         NAMES_LIST[i] = (char) str;
-        printf("%s", get_name());
-        i++;
-
+        printf("%c", get_name());
+        k++;
     }
+
+
     return 0;
 }
--- get_name.h
+++ get_name.h
@@ -1,15 +1,32 @@
 
 #ifndef MMN12_GET_NAME_H
 #define MMN12_GET_NAME_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <time.h>
 
-char get_name(){}
+#define NULL_TERMINATED_STRING '\0'
+char NAMES_LIST[30];
 
-bool contains(char str, int i){}
+
+
+/*!
+ *
+ * @return
+ */
+char get_name();
+
+/*!
+ *
+ * @param str
+ * @param i
+ * @return
+ */
+bool contains(char str[], int i);
 
 
 
