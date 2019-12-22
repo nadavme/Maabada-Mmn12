61956af6c811797d640adb06845ecbc59098a911
Nadav Meidan <nadav.medan@mail.huji.ac.il>
2019-12-22 17:20:08 +0200

    finished documentation

--- get_name.c
+++ get_name.c
@@ -22,10 +22,12 @@ char*  get_name()
 bool contains(char str[], int i)
 {
     int j, k;
+    /*run all over the string*/
     for (k = 0; k < (int) strlen(str); k++)
     {
         str[k] = tolower(str[k]);
     }
+    /*checks if the name is already exists in the list*/
     for (j = 0; j < i; j++)
     {
         if(strcmp(NAMES_LIST[j], str) == 0) return true;
@@ -39,30 +41,28 @@ bool contains(char str[], int i)
  * @return
  */
 int main() {
-    char str[20];
-    int i, k;
-    char* TheChosenName;
+    char str[MAX_STRING];
+    int i;
     printf("Please enter 30 names: ");
-    for (i = 0; i < 30; i++)
+    for (i = 0; i < MAX_NAMES; i++)
     {
         scanf("%s", str);
-        printf("%s ", str);
+        printf("%s ", str);/*prints immediately the input*/
         /*ensure 'str' doesn't holds '\n' or ' '*/
         if(str[strlen(str)-1] == '\n' || str[strlen(str)-1] == ' ') str[strlen(str)-1] = NULL_TERMINATED_STRING;
         if (contains(str, i))
         {
-
+            /*breaks with an informative message*/
             printf("\n\nOne of the names was entered twice. The program will shut down now\n");
             exit(11);
-        }
+        }/*add the new name to the list*/
         else strcpy(NAMES_LIST[i], str);
-    }
+    }/*calls get_name 10 times, and prints the chosen names*/
     printf("\n\n The names that were chosen randomly are:\n");
-    srand(time(NULL));
-    for (i = 0; i < 10; i++)
+    srand(time(NULL));/*so rand will be efficient*/
+    for (i = 0; i < NUM_OF_CALLS; i++)
     {
-        TheChosenName = get_name();
-        printf("\n%s ", TheChosenName);
+        printf("\n%s ", get_name());
     }
     return 0;
 }
--- get_name.h
+++ get_name.h
@@ -10,21 +10,26 @@
 #include <time.h>
 
 #define NULL_TERMINATED_STRING '\0'
+#define MAX_STRING  20
+#define MAX_NAMES  30
+#define NUM_OF_CALLS 10
+
 char NAMES_LIST[30][20];
 
 
 
 /*!
- *
- * @return
+ * * The function pull a number between 1-30, and return the name that in that number place in the NAMES_LIST.
+ * @return the name that came out in the draw.
  */
 char* get_name();
 
 /*!
- *
- * @param str
- * @param i
- * @return
+ *The function takes the string from the stdin and make all its letters lowercase, and then checks if that name is
+ * already at the list. if not, it will add it to the list.
+ * @param str is the string from stdin.
+ * @param i is the number of elements that are already in the list.
+ * @return True if the list contains the new name, anf False else.
  */
 bool contains(char str[], int i);
 
